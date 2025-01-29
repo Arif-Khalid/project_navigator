84cec53fde7df86f1d1965f636f1ae9096dd236f
arif <arifkhalid99@gmail.com>
2025-01-29 19:12:09 +0800

    Shift configuration to separate text file

--- pn.sh
+++ pn.sh
@@ -2,7 +2,13 @@
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
 RESET=$(tput sgr0)
+DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECTS_FILE_NAME="projects.txt"
+SHELL_FILE_NAME="pn.sh"
+
 # Remember to call this script with . or source in order to change directory in the calling terminal
 # For example: source pn.sh
 # 
@@ -14,23 +20,35 @@ RESET=$(tput sgr0)
 # "/etc/profile", "~/bash_profile", "~/.bash_login" or "~/.profile"
 
 # Variables to edit
-projectNames=("Neovim Config" "Project 1" "Project 2") # Project names to identify your projects
-projectPaths=("$HOME/.config/nvim" "$HOME/Desktop/Project_1" "$HOME/Desktop/Project_2") # The paths of the respective project in the same order
+IFS='='
+projectNames=()
+projectPaths=()
+while read -a project; do
+    echo ${project[0]}
+    echo ${project[1]}
+    if [ ! ${#project[@]} -eq 2 ]; then
+        echo "${RED}Field starting with ${CYAN}${project}${RED} in ${BLUE}${DIR_NAME}/${PROJECTS_FILE_NAME}${RED} not written properly${RESET}"
+        return
+    fi
+    projectNames+=("${project[0]}")
+    projectPaths+=("${project[1]}")
+
+done < "${DIR_NAME}/${PROJECTS_FILE_NAME}"
+
+#projectNames=("Neovim Config" "Project 1" "Project 2") # Project names to identify your projects
+#projectPaths=("$HOME/.config/nvim" "$HOME/Desktop/Project_1" "$HOME/Desktop/Project_2") # The paths of the respective project in the same order
 
 names_length=${#projectNames[@]}
 paths_length=${#projectPaths[@]}
 if [ ! $names_length -eq $paths_length ]; then
     echo "${RED}The project names and paths have different lengths"
-    echo "Please edit pn.sh to include the same number of items in each array${RESET}"
+    echo "Please edit ${BLUE}${DIR_NAME}/${SHELL_FILE_NAME}${RED} to include the same number of items in each array${RESET}"
     return
 fi
 numberRegex='^[0-9]+$'
 
 echo "Hello and welcome to your repo navigator!"
 echo "Which project would you like to navigate to?"
-
-
-
 option_valid=false
 
 while [[ "$option_valid" = false ]]; do
@@ -48,10 +66,10 @@ while [[ "$option_valid" = false ]]; do
 
 done
 if [[ ! -d "${projectPaths[$option]}" ]]; then
-    echo "${RED}The directory for ${projectNames[$option]}: ${projectPaths[$option]} does not exist"
-    echo "Please edit pn.sh to point to a valid directory${RESET}"
+    echo "${RED}The directory for ${CYAN}${projectNames[$option]}: ${BLUE}${projectPaths[$option]}${RED} does not exist"
+    echo "Please edit ${BLUE}${DIR_NAME}/${SHELL_FILE_NAME}${RED} to point to a valid directory${RESET}"
     return
 fi 
 cd ${projectPaths[$option]}
-echo "${GREEN}You are now in the directory for ${projectNames[$option]}"
+echo "${GREEN}You are now in the directory for ${CYAN}${projectNames[$option]}${GREEN}"
 echo "Use ${YELLOW}nvim${GREEN} to get started${RESET}"
