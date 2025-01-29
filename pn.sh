1c0195c15e35edcf873314ce7c47a29d70f347d9
arif <arifkhalid99@gmail.com>
2025-01-29 18:06:02 +0800

    Add error handling for invalid paths

--- pn.sh
+++ pn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
 # Remember to call this script with . or source in order to change directory in the calling terminal
 # For example: source pn.sh
 # 
@@ -14,17 +17,19 @@
 projectNames=("Neovim Config" "Project 1" "Project 2") # Project names to identify your projects
 projectPaths=("$HOME/.config/nvim" "$HOME/Desktop/Project_1" "$HOME/Desktop/Project_2") # The paths of the respective project in the same order
 
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-RESET=$(tput sgr0)
+names_length=${#projectNames[@]}
+paths_length=${#projectPaths[@]}
+if [ ! $names_length -eq $paths_length ]; then
+    echo "${RED}The project names and paths have different lengths"
+    echo "Please edit pn.sh to include the same number of items in each array${RESET}"
+    return
+fi
 numberRegex='^[0-9]+$'
 
 echo "Hello and welcome to your repo navigator!"
 echo "Which project would you like to navigate to?"
 
 
-array_length=${#projectNames[@]}
 
 option_valid=false
 
@@ -36,14 +41,17 @@ while [[ "$option_valid" = false ]]; do
 
     read -p "Option index: " option
 
-    if ! [[ $option =~ $numberRegex ]] || [ $option -ge $array_length ] || [ $option -lt 0 ]; then
+    if ! [[ $option =~ $numberRegex ]] || [ $option -ge $names_length ] || [ $option -lt 0 ]; then
         echo "${RED}Your chosen option is invalid, please select again${RESET}"
         option_valid=false
     fi
 
 done
-
+if [[ ! -d "${projectPaths[$option]}" ]]; then
+    echo "${RED}The directory for ${projectNames[$option]}: ${projectPaths[$option]} does not exist"
+    echo "Please edit pn.sh to point to a valid directory${RESET}"
+    return
+fi 
 cd ${projectPaths[$option]}
 echo "${GREEN}You are now in the directory for ${projectNames[$option]}"
 echo "Use ${YELLOW}nvim${GREEN} to get started${RESET}"
-
